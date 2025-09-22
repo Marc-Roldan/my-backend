cc71378b2d63c5af8ccce8186cf425350f6ef788
Marc <marcchristian.roldan@gmail.com>
2025-09-22 08:39:24 +0800

    Fix /hello route

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(cors());
 app.use(express.json());
 
 // Example route
-app.get("/api/hello", (req, res) => {
+app.get("/hello", (req, res) => {
   res.json({ message: "Hello from Railway Backend!" });
 });
 
